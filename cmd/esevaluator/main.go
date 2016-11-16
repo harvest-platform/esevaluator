6a656b632b868bc4d8fd466405530fb4305fe00f
gerpsh <g@gerpsh.me>
2016-11-16 15:50:10 -0500

    Add TLS support
    Addresses #7

--- cmd/esevaluator/main.go
+++ cmd/esevaluator/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -8,6 +9,25 @@ import (
 )
 
 func main() {
+	log.SetFlags(0)
+
+	var (
+		httpaddr string
+		tlscert  string
+		tlskey   string
+	)
+
+	flag.Parse()
+
+	flag.StringVar(&httpaddr, "http", "127.0.0.1:8080", "Address for HTTP transport.")
+	flag.StringVar(&tlscert, "tlscert", "", "Path to TLS certificate.")
+	flag.StringVar(&tlskey, "tlskey", "", "Path to TLS key.")
+
 	http.Handle("/elastic", transport.TranslateHandler())
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	if tlscert == "" {
+		log.Fatal(http.ListenAndServe(httpaddr, nil))
+	} else {
+		log.Fatal(http.ListenAndServeTLS(httpaddr, tlscert, tlskey, nil))
+	}
 }
